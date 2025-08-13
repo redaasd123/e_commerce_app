67a5419db727c4c1a3b25e8073f1f3f1b30ca863
Reda Hassan <redah4142@gmail.com>
2025-08-13 19:28:45 +0300

    cashed data

---
+++ lib/feature/home/domain/entity/cart_entity/cart_entity.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cart_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CartEntityAdapter extends TypeAdapter<CartEntity> {
+  @override
+  final int typeId = 0;
+
+  @override
+  CartEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return CartEntity(
+      id: fields[0] as int,
+      products: (fields[1] as List).cast<ProductEntity>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, CartEntity obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.products);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CartEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
---
+++ lib/feature/home/domain/entity/product_entity/product_entity.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProductEntityAdapter extends TypeAdapter<ProductEntity> {
+  @override
+  final int typeId = 1;
+
+  @override
+  ProductEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ProductEntity(
+      id: fields[0] as int,
+      title: fields[1] as String,
+      thumbnail: fields[2] as String,
+      price: fields[3] as num,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ProductEntity obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.thumbnail)
+      ..writeByte(3)
+      ..write(obj.price);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProductEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
